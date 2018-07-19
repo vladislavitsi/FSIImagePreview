6e588164b91736827b66164882ae797fb70c9075
Uladzislau Kleshchanka <vladislavitsi@gmail.com>
2018-07-19 09:52:42 +0300

    Initial Commit

---
+++ FSIImagePreview/FSIImagePreview.h
@@ -0,0 +1,19 @@
+//
+//  FSIImagePreview.h
+//  FSIImagePreview
+//
+//  Created by Uladzislau Kleshchanka on Jul/19/2018.
+//  Copyright © 2018 Uladzislau Kleshchanka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FSIImagePreview.
+FOUNDATION_EXPORT double FSIImagePreviewVersionNumber;
+
+//! Project version string for FSIImagePreview.
+FOUNDATION_EXPORT const unsigned char FSIImagePreviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSIImagePreview/PublicHeader.h>
+
+
